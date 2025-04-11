a267d7dc370edffb5f138d842cd3c42e189aa76b
kaydellion <kanyin500@gmail.com>
2025-04-12 00:44:27 +0100

    fix: add notification message for empty notifications and enhance gender selection input

--- settings.php
+++ settings.php
@@ -51,7 +51,7 @@
                                 <input type="text" class="form-control" name="mobile_number" placeholder="Phone Number" value="<?php echo htmlspecialchars($mobile_number); ?>" required>
                             </div>
                             <div class="col-md-6 form-group p_star mb-3">
-<select name="gender">
+                            <select class="form-control" id="gender" name="gender" required>
 <option value="" <?php echo empty($gender) ? 'selected' : ''; ?>>-Select Gender-</option>
 <option value="male" <?php echo ($gender == 'Male') ? 'selected' : ''; ?>>Male</option>
 <option value="female" <?php echo ($gender == 'Female') ? 'selected' : ''; ?>>Female</option>
