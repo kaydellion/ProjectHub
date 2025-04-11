db84472b80b834a17537ae6bb71d2673a6f84294
kaydellion <kanyin500@gmail.com>
2025-04-11 22:27:26 +0100

    feat: update email templates and add admin name for improved branding

--- backend/functions.php
+++ backend/functions.php
@@ -188,14 +188,14 @@ function sendEmail($vendorEmail, $vendorName, $siteName, $siteMail, $emailMessag
    $email_to = $vendorEmail;
    $email_subject = "$emailSubject - $siteName";
    $email_message = "<div style='width:600px; padding:100px 60px; background-color:#000080; color:#fff;'>
-   <p><img src='https://$adminlink/images/$siteimg' style='width:10%; height:auto;' /></p>
+   <p><img src='$siteurl/uploads/$siteimg' style='width:10%; height:auto;' /></p>
    <p style='font-size:14px; color:#fff;'> <span style='font-size:14px; color:#89CFF0;'>Hello there, $vendorName,</span>
    $emailMessage</p>
    <p><a href='$siteurl' style='font-size:14px; padding-top:20px;  font-weight:600; color:#89CFF0;'>VISIT THE WEBSITE</a></p>
    </div>";
 
    // create email headers
-   $header = 'From: "' . $siteName . '" <' . $siteMail . '>' . "\r\n";
+   $header = 'From: ' . $siteName . ' <' . $siteMail . '>' . "\r\n";
    $header .= "Cc:$siteMail \r\n";
    $header .= 'Reply-To: ' . $siteMail . '' . "\r\n";
    $header .= "MIME-Version: 1.0\r\n";
@@ -228,7 +228,7 @@ function sendEmail2($vendorEmail, $vendorName, $siteName, $siteMail, $emailMessa
     $email_body .= "Content-Type: text/html; charset=UTF-8\r\n";
     $email_body .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
     $email_body .= "<div style='width:600px; padding:40px; background-color:#000080; color:#fff;'>
-                        <p><img src='https://$adminlink/images/$siteimg' style='width:10%; height:auto;' /></p>
+                        <p><img src='$siteurl/uploads/$siteimg' style='width:10%; height:auto;' /></p>
                         <p style='font-size:14px; color:#fff;'>
                             <span style='font-size:14px; color:#89CFF0;'>Hello, $vendorName,</span><br>
                             $emailMessage
