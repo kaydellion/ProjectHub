5e9bd6f8b3e44a6df67cdedfa810f021125bd470
kaydellion <kanyin500@gmail.com>
2025-04-12 01:13:25 +0100

    feat: remove unnecessary id attribute from contact form

--- contact.php
+++ contact.php
@@ -51,7 +51,7 @@
           <h2 class="contact-title">Get in Touch</h2>
         </div>
         <div class="col-lg-8">
-          <form class="form-contact contact_form"  method="post" id="contactForm">
+          <form class="form-contact contact_form"  method="post">
             <div class="row">
               <div class="col-12">
                 <div class="form-group">
