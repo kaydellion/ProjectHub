6c172c21cadd9d68c99c17cf0252b9a8d0f93cf4
kaydellion <kanyin500@gmail.com>
2025-04-12 01:28:53 +0100

    feat: add site bank details and Google Map input fields to settings form

--- admin/pages/actions.php
+++ admin/pages/actions.php
@@ -687,6 +687,10 @@ if(isset($_POST['settings'])){
     $number = $_POST['site_number'];
     $profilePicture = $_FILES['site_logo'];
 
+    $site_bank= $_POST['site_bank'];
+    $account_name= $_POST['account_name'];
+    $account_number= $_POST['account_number'];
+    $google= $_POST['google_map'];
 
     $uploadDir = '../../img/';
     $fileKey='site_logo';
@@ -700,7 +704,7 @@ if(isset($_POST['settings'])){
     }
 
   
-    $update = mysqli_query($con,"UPDATE " . $siteprefix . "site_settings SET site_name='$name', site_keywords='$keywords', site_url='$url', site_description='$description', site_logo='$logo', site_mail='$email', site_number='$number' WHERE s=1");
+    $update = mysqli_query($con,"UPDATE " . $siteprefix . "site_settings SET site_name='$name',site_bank='$site_bank', account_name='$account_name', account_number='$account_number', google_map='$google',  site_logo='$logo',  site_keywords='$keywords', site_url='$url', site_description='$description', site_mail='$email', site_number='$number' WHERE s=1");
 
 
     if($update){
