6766e4d3a40235cc794f667762a1261bdaaf05cf
fopsycute <fopsycute18@gmail.com>
2025-04-27 20:36:38 +0100

    pull

--- backend/functions.php
+++ backend/functions.php
@@ -11,7 +11,7 @@ function getCartCount($con, $siteprefix, $order_id) {
 
 function checkActiveLog($active_log) {
     if ($active_log == "0") {
-        header("location: signin.php");
+        header("location: $siteurl.signin.php");
         exit(); // Make sure to exit after the redirect
     }
 }
