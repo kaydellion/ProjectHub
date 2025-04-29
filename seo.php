27ed912f431024c72a5f223d7731199e8e9bf1a2
kaydellion <kanyin500@gmail.com>
2025-04-30 00:11:53 +0100

    run

--- seo.php
+++ seo.php
@@ -6,14 +6,14 @@ $conn = mysqli_connect ("$db_host","$db_username","$db_pass","$db_name");
 if (isset($_GET['slugs'])) {
     $raw_slug = $_GET['slugs'];
     $title_like = str_replace('-', ' ', $raw_slug);
-    $category_name = mysqli_real_escape_string($conn, $title_like);
+    $category_name = mysqli_real_escape_string($conn, ucwords($title_like));
 }
 $category_titles = [];
 $sql = "SELECT * FROM pr_resource_types WHERE parent_id IS NULL";
 $sql2 = mysqli_query($conn, $sql);
 
 while ($row = mysqli_fetch_array($sql2)) {
-    $category_titles[] = $row['name'] . ' on'. $category_name;
+    $category_titles[] = $row['name'] . ' on '. $category_name;
 }
 
 // Join titles with a pipe separator
