f49b406161908b72179ebfb2bd9268b413b457e5
kaydellion <kanyin500@gmail.com>
2025-04-30 19:50:20 +0100

    load

--- backend/datatable.php
+++ backend/datatable.php
@@ -3,22 +3,22 @@
 <script src="https://cdn.datatables.net/2.2.1/js/dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/2.2.1/js/dataTables.bootstrap5.min.js"></script>
 <script>
-  $(document).ready(function() {
-    $('.table').each(function() {
-        if ($(this).find('tbody tr').length > 1) { // Ensure the table has at least one row
-            $(this).DataTable({
-                "paging": true,
-                "searching": true,
-                "order": [],
-                "info": true,
-                "lengthMenu": [10, 25, 50, 100],
-                "autoWidth": false,
-                "language": {
-                    "zeroRecords": "No records available",
-                    "emptyTable": "No data available in table"
-                }
-            });
+ $(document).ready(function() {
+  $('.table').each(function() {
+    if ($(this).find('tbody tr').length > 1) {
+      $(this).DataTable({
+        "paging": true,
+        "searching": true,
+        "info": true,
+        "lengthMenu": [10, 25, 50, 100],
+        "autoWidth": false,
+        "order": [], // Disable auto sorting on page load
+        "language": {
+          "zeroRecords": "No records available",
+          "emptyTable": "No data available in table"
         }
-    });
+      });
+    }
+  });
 });
 </script>
