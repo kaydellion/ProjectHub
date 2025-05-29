919dcfab45653d74ef778257873d649696b67ca4
fopsycute <fopsycute18@gmail.com>
2025-05-29 17:32:12 +0100

    feat: move delete record logic from actions.php to delete.php

--- admin/pages/delete.php
+++ admin/pages/delete.php
@@ -1,7 +1,25 @@
-<?php include "header.php"; ?>
+<?php
+
+include "../../backend/functions.php";  
+include "../../backend/connect.php";  
+
+
+//delete-record
+  if (isset($_GET['action']) && $_GET['action'] == 'delete') {
+    $table = $_GET['table'];
+    $item = $_GET['item'];
+    $page = $_GET['page'];
+    
+    if (deleteRecord($table, $item)) {
+        $message="Record deleted successfully.";
+    } else {
+         $message="Failed to delete the record.";
+    }
+
+    showToast($message);
+    header("refresh:1; url=$page");
+}
 
 
 
-
-
-<?php include "footer.php"; ?>
+?>
