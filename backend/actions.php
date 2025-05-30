9928a67c9531979a9f09ddf7a6d44cf555a67e9b
kaydellion <kanyin500@gmail.com>
2025-05-30 23:43:40 +0100

    w

--- backend/actions.php
+++ backend/actions.php
@@ -955,7 +955,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'delete') {
     $page = $_GET['page'];
     
     if (deleteRecord($table, $item)) {
-        $message="Record deleted successfully.";
+        $message="Record deleted successfully. $page will be refreshed in 2 seconds.";
     } else {
          $message="Failed to delete the record.";
     }
